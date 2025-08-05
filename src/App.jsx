ad8e359065552ac073a32b5de97c52eb2801d46a
Vamsibablu <ovamsikrishna2000@gmail.com>
2025-08-05 12:45:41 +0530

    context-react-book-examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import DebounceFn from "./components/debounce";
 import ParentFn from "./components/parent";
@@ -5,17 +6,22 @@ import User1Provider from "./context/contextProvider1";
 import User2Provider from "./context/contextProvider2";
 
 function App() {
+  const [appState, setAppState] = useState(false); // this state change triggers entire app to re-render even the children
   console.log("app");
   return (
     <>
+      <button
+        onClick={() => {
+          setAppState(!appState);
+        }}
+      >
+        Change App State
+      </button>
       <User1Provider>
         <User2Provider>
           <ParentFn />
         </User2Provider>
       </User1Provider>
-
-      {/* debouncing */}
-      {/* <DebounceFn /> */}
     </>
   );
 }
--- src/context/contextProvider1.jsx
+++ src/context/contextProvider1.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import React from "react";
 
 export const User1Context = React.createContext();
 
 export default function User1Provider({ children }) {
   const [user1, setUser1] = useState(0);
+  const [independetState, setIndState] = useState(false);
+  useEffect(() => {
+    console.log("user1 function changed"); // just checking if setuser1 is chaning every time or not
+  }, [setUser1]);
+  // we need to memoize the value because changing independent state also change value object hence it rerenders its consuming components
+  const value = useMemo(() => {
+    return { user1, setUser1 };
+  }, [user1, setUser1]);
+
   console.log("context1 provider");
+  console.log(independetState, "independent state");
   return (
-    <User1Context.Provider value={{ user1, setUser1 }}>
+    <User1Context.Provider value={value}>
+      <button
+        onClick={() => {
+          setIndState(!independetState);
+        }}
+      >
+        Change the independent state
+      </button>
+
       {/* Children wont re render on changing state of user1 */}
       {children}
     </User1Context.Provider>
